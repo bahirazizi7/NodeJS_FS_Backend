449eb4f49fa603b361801cc6d8ffa9b83a1d9b7b
Bahir Azizi <bahirazizi7@gmail.com>
2024-03-23 01:14:05 +0500

    Requst Listener

---
+++ app/app.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+// Use middleware for contracting the json payload
+app.use(express.json())
+
+// Use middleware for url encoding 
+app.use(express.urlencoded({ extended: true }))
+
+// Use middleware to handle the cors policy 
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Server is up" })
+})
+
+//Routers
+
+
+// Bad url error handling with middleware
+app.use((req, res, next) => {
+    const error = new Error("Not Found")
+    error.status = 404
+    next(error)
+})
+// catch up the error 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status
+        }
+    })
+
+})
+
+module.exports = app
